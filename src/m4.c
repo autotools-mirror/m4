97ab5dc0c4bccedd05b58cc873279eff06a356b2
Gary V. Vaughan <gary@gnu.org>
2004-09-09 21:10:26 +0000

    * configure.in (AC_CHECK_HEADERS):  Add signal.h, sys/signal.h. * src/m4.c: And include them as appropriate. * NEWS: Updated.

--- src/m4.c
+++ src/m4.c
@@ -2,17 +2,17 @@
 
    Copyright (C) 1989, 1990, 1991, 1992, 1993, 1994, 2004 Free
    Software Foundation, Inc.
-  
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -21,7 +21,13 @@
 #include "m4.h"
 
 #include <getopt.h>
-#include <sys/signal.h>
+
+#if defined(HAVE_SYS_SIGNAL_H)
+#  include <sys/signal.h>
+#endif
+#if defined(HAVE_SIGNAL_H)
+#  include <signal.h>
+#endif
 
 static void usage _((int));
 
