a27777dd1088943520c2c8a8fd0f59a504a15dd0
Gary V. Vaughan <gary@gnu.org>
2005-05-06 09:05:44 +0000

    * src/m4.h (EXIT_SUCCESS, EXIT_FAILURE): Removed.  These are handled already by gnu/exit.h.

--- src/m4.h
+++ src/m4.h
@@ -73,14 +73,6 @@ long strtol ();
 
 #endif /* STDC_HEADERS */
 
-/* Some systems do not define EXIT_*, even with STDC_HEADERS.  */
-#ifndef EXIT_SUCCESS
-# define EXIT_SUCCESS 0
-#endif
-#ifndef EXIT_FAILURE
-# define EXIT_FAILURE 1
-#endif
-
 #if HAVE_ERRNO_H
 #include <errno.h>
 #endif
