c1841dc6d2e95ebe4f4c705a36254447957152c2
Eric Blake <ebb9@byu.net>
2007-07-22 04:40:15 +0000

    Fix regression on NetBSD from 2007-05-28. * src/output.c (m4_tmpopen): Explicitly reset append-mode stream position to byte 0. * NEWS: Document this fix. * THANKS: Update. Reported by Thomas Klausner.

--- src/output.c
+++ src/output.c
@@ -252,6 +252,11 @@ m4_tmpopen (int divnum)
   else if (set_cloexec_flag (fileno (file), true) != 0)
     M4ERROR ((warning_status, errno,
 	      "Warning: cannot protect diversion across forks"));
+  /* POSIX states that it is undefined whether an append stream starts
+     at offset 0 or at the end.  We want the beginning.  */
+  else if (fseeko (file, 0, SEEK_SET) != 0)
+    M4ERROR ((EXIT_FAILURE, errno,
+	      "cannot seek to beginning of diversion"));
   return file;
 }
 
