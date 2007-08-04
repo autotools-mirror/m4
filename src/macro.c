486fb4ada13ea82fc444b6eb913ac19f1e0e37ef
Eric Blake <ebb9@byu.net>
2007-08-04 20:40:08 +0000

    Normalize all GPL license notices. * GNUmakefile: Update license wording. * Makefile.am: Likewise. * Makefile.maint: Likewise. * bootstrap: Likewise. * commit: Likewise. * configure.ac: Likewise. * checks/Makefile.in: Likewise. * doc/Makefile.am: Likewise. * examples/Makefile.am: Likewise. * src/Makefile.am: Likewise. * src/builtin.c: Likewise. * src/debug.c: Likewise. * src/eval.c: Likewise. * src/format.c: Likewise. * src/freeze.c: Likewise. * src/input.c: Likewise. * src/m4.c: Likewise. * src/m4.h: Likewise. * src/macro.c: Likewise. * src/output.c: Likewise. * src/path.c: Likewise. * src/stackovf.c: Likewise. * src/symtab.c: Likewise.

--- src/macro.c
+++ src/macro.c
@@ -3,20 +3,20 @@
    Copyright (C) 1989, 1990, 1991, 1992, 1993, 1994, 2006, 2007 Free
    Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This file is part of GNU M4.
+
+   GNU M4 is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
+   the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   GNU M4 is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301  USA
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /* This file contains the functions, that performs the basic argument
