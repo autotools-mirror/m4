359f9f0c4c790aeceedb395eb11813b388cfe60c
Eric Blake <ebb9@byu.net>
2008-03-28 06:44:20 -0600

    Use GNUmakefile module.
    * ltdl/m4/gnulib-cache.m4: Import gnumakefile module.
    * Makefile.am: Split off maintainer rules...
    * maint.mk: ...into this new file.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

---
+++ maint.mk
@@ -0,0 +1,54 @@
+## maint.mk - maintainer rules for managing m4 distributions
+##
+## Copyright (C) 2000, 2001, 2003, 2004, 2005, 2006, 2007, 2008 Free
+## Software Foundation, Inc.
+##
+## This file is part of GNU M4.
+##
+## GNU M4 is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## GNU M4 is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##                                                                      ##
+##                   --- RULES FOR THE MAINTAINER ---                   ##
+##                                                                      ##
+
+## TODO - fix this file now that m4 is maintained in git, not CVS.
+
+CVS	= cvs # set it to `:' to avoid CVS operations at dist time
+
+# Tag before making distribution.  Also, don't make a distribution if
+# checks fail.  Also, make sure the NEWS file is up-to-date.
+cvs-dist: distcheck # maintainer-check
+	@if sed '1,2d;3q' $(srcdir)/NEWS | grep -e "$(VERSION)" > dev/null; \
+	  echo "NEWS not updated; not releasing" 1>&2; \
+	  exit 1;
+	fi
+	cd $(srcdir) && \
+	  $(CVS) -q tag `echo "Release-$(VERSION)" | sed 's/\./-/g'`
+	$(MAKE) dist
+
+cvs-diff:
+	thisver=`echo "Release-$(VERSION)" | sed 's/\./-/g'`; \
+	if test -z "$$OLDVERSION"; then \
+	  prevno=`echo "$(VERSION)" - 0.01 | bc | sed 's/^\./0./'`; \
+	else prevno="$$OLDVERSION"; fi; \
+	prevver=Release-`echo $$prevno | sed 's/\./-/g'`; \
+	cvs -f rdiff -c -r $$prevver -r $$thisver $(PACKAGE) \
+	    > $(PACKAGE)-$$prevno-$(VERSION).diff
+
+.PHONY: web-manual
+web-manual:
+	@cd $(srcdir)/doc; \
+	  $(SHELL) "$(abs_srcdir)/$(config_aux_dir)/gendocs.sh" $(PACKAGE) \
+	    "GNU $(PACKAGE)- GNU macro processor"
+	@echo " *** Upload the doc/manual directory to web-cvs."
