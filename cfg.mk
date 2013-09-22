de03cc2c03f81204052aa49bc22b397d0e7a9b62
Gary V. Vaughan <gary@gnu.org>
2013-09-22 14:04:20 +0700

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -33,7 +33,7 @@ local-checks-to-skip += sc_cast_of_x_alloc_return_value
 config_h_header = "m4\.h"
 
 # Hash of NEWS contents, to ensure we don't add entries to wrong section.
-old_NEWS_hash = 345b93b3900db54f6bab85e3da153b38
+old_NEWS_hash = ef92e5b599b23d9dfe915db5ce4194c5
 
 # Indent only with spaces.
 sc_prohibit_tab_based_indentation:
