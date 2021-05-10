b248d8c388fd3482de5164d62ecfb33bbbb3ae23
Bruno Haible <bruno@clisp.org>
2021-05-10 13:26:33 -0500

    eval: avoid undefined behaviour when parsing -2147483648
    * src/eval.c (eval_lex): Use an unsigned variable for accumulating the
    value.
    https://lists.gnu.org/archive/html/bug-m4/2021-05/msg00001.html
    
    (cherry picked from commit 61f8b953332e7730b72690a5eda706d4f134a229)

--- src/eval.c
+++ src/eval.c
@@ -122,7 +122,11 @@ eval_lex (int32_t *val)
 
   if (c_isdigit (*eval_text))
     {
-      int base, digit;
+      unsigned int base, digit;
+      /* The documentation says that "overflow silently results in wraparound".
+         Therefore use an unsigned integer type to avoid undefined behaviour
+         when parsing '-2147483648'.  */
+      uint32_t value;
 
       if (*eval_text == '0')
         {
@@ -159,8 +163,7 @@ eval_lex (int32_t *val)
       else
         base = 10;
 
-      /* FIXME - this calculation can overflow.  Consider xstrtol.  */
-      *val = 0;
+      value = 0;
       for (; *eval_text; eval_text++)
         {
           if (c_isdigit (*eval_text))
@@ -175,8 +178,8 @@ eval_lex (int32_t *val)
           if (base == 1)
             {
               if (digit == 1)
-                (*val)++;
-              else if (digit == 0 && !*val)
+                value++;
+              else if (digit == 0 && value == 0)
                 continue;
               else
                 break;
@@ -184,8 +187,9 @@ eval_lex (int32_t *val)
           else if (digit >= base)
             break;
           else
-            *val = *val * base + digit;
+            value = value * base + digit;
         }
+      *val = value;
       return NUMBER;
     }
 
